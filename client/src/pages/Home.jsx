348094c05b16e69270433e23d96c52b0191d82f6
Kumari neha rani <1.2ranineha456@gmail.com>
2025-08-27 17:14:32 +0530

    added Testimony  section in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Hero from '../components/Hero.jsx'
 import AiTools from '../components/Aitools.jsx'
-
+import Testimonial from '../components/Testimonial.jsx'
 const Home = () => {
   return (
     <>
       <Navbar/>
       <Hero/>
       <AiTools/>
+      <Testimonial/>
 
     </>
   )
