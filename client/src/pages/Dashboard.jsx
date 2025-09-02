8f14b9f68271c83a92eb5173fb5f458d27d62488
Kumari neha rani <1.2ranineha456@gmail.com>
2025-09-02 18:41:31 +0530

    created recent creation and active plan card in dashbord.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { dummyCreationData } from "../assets/assets";
+import { Gem, Sparkle } from "lucide-react";
+import { Protect } from "@clerk/clerk-react";
 
 const Dashboard = () => {
-  return (
-    <div>
-        <h1> Dashboard</h1>
-    </div>
-  )
-}
+  const [creation, setCreation] = React.useState([]);
 
-export default Dashboard
+  const getDashboardData = async () => {
+    setCreation(dummyCreationData); // fixed case
+  };
+
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+
+  return (
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap">
+        {/* total card creation */}
+        <div
+          className="flex justify-between items-center w-72 p-4 px-6 bg-white
+rounded-xl border border-gray-200"
+        >
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creations</p>
+            <h2 className="text-xl font-semibold">{creation.length}</h2>
+          </div>
+          <div
+            className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2]
+  to-[#0BB0D7] text-white flex justify-center items-center"
+          >
+            <Sparkle className="w-5 text-white" />
+          </div>
+        </div>
+        {/* Active plan card */}
+        <div
+          className="flex justify-between items-center w-72 p-4 px-6 bg-white
+rounded-xl border border-gray-200"
+        >
+          <div className="text-slate-600">
+            <p className="text-sm">Active Plan </p>
+            <h2 className="text-xl font-semibold">
+              <Protect plan ="premium" fallback="Free">
+                Premium
+              </Protect>
+            </h2>
+          </div>
+          <div
+            className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5]
+  to-[#9E53EE] text-white flex justify-center items-center"
+          >
+            <Gem className="w-5 text-white" />
+          </div>
+        </div>
+      </div>
+
+    <div className="space-y-3">
+      <p className="mt-6 mb-4">
+        Recent Creation
+      </p>
+
+    </div>
+
+    </div>
+  );
+};
+
+export default Dashboard;
