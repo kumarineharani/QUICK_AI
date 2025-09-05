0e69beaf7e0285f790a855a4d39487c5f11e55d1
Kumari neha rani <1.2ranineha456@gmail.com>
2025-09-05 07:25:41 +0530

    create write article ui page

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,4 +1,4 @@
-import { Sparkle } from "lucide-react";
+import { Edit, Sparkle } from "lucide-react";
 import React from "react";
 
 const WriteArticle = () => {
@@ -10,11 +10,17 @@ const WriteArticle = () => {
 
   const [selectedLength, setSelectedLength] = React.useState(articleLength[0]);
   const [input, setInput] = React.useState("");
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
       {/* left col */}
-      <form className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
         <div className="flex items-center gap-3">
           <Sparkle className="w-6 text-[#4A7AFF]" />
           <h2 className="text-xl font-semibold">Article Configuration</h2>
@@ -22,6 +28,8 @@ const WriteArticle = () => {
 
         <p className="mt-6 text-sm font-medium">Article Topic</p>
         <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
           type="text"
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border"
           placeholder="The future of artificial intelligence is ..."
@@ -29,22 +37,49 @@ const WriteArticle = () => {
         />
 
         <p className="mt-4 text-sm font-medium">Article Length</p>
-        <div>
+        <div className="flex gap-2 mt-2 flex-wrap">
           {articleLength.map((item, index) => (
             <span
               onClick={() => setSelectedLength(item)}
               className={`text-xs px-4 py-1 border rounded-full cursor-pointer
-                 ${selectedLength.text === item.text ? 'bg-blue-50 texr-blue700' : 'text-gray-500 border-gray-300'} `}
+                ${
+                  selectedLength.text === item.text
+                    ? "bg-blue-50 text-blue-700 border-blue-300"
+                    : "text-gray-500 border-gray-300"
+                } `}
               key={index}
             >
               {item.text}
             </span>
           ))}
         </div>
+
+        <button
+          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF]
+          to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
+        >
+          <Edit className="w-5" />
+          Generate Article
+        </button>
       </form>
 
       {/* right col */}
-      <div>{/* future content */}</div>
+      <div
+        className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border 
+       border-gray-200 min-h-96 max-h-[600px]"
+      >
+        <div className="flex items-center gap-3">
+          <Edit className="w-5 h-5 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Generated article</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Edit className="w-9 h-9" />
+            <p>Enter a topic and click "Generate article" to get started</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
