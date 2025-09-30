e1955fd2962b930556f4dcf740021e5361ea5795
Igneel-98 <ghostriderattmd@gmail.com>
2025-09-30 14:11:31 +0530

    fix: correct import path for Footer component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from '../components/Hero.jsx'
 import AiTools from '../components/AiTools.jsx'
 import Testimonial from '../components/Testimonial.jsx'
 import Plan from '../components/Plan.jsx'
-import Footer from '../components/footer.jsx'
+import Footer from '../components/Footer.jsx'
 const Home = () => {
   return (
     <>
